2bdc8832cce07ee2fa53d9df1b60d2e38c362991
nd <nd1012@users.noreply.github.com>
2023-05-20 09:46:55 +0200

    Update
    + Added numeric bitwise extensions

---
+++ src/Wan24-Core/BitwiseInt16Extensions.cs
@@ -0,0 +1,97 @@
+﻿namespace wan24.Core
+{
+    /// <summary>
+    /// Bitwise Int16 extensions
+    /// </summary>
+    public static class BitwiseInt16Extensions
+    {
+        /// <summary>
+        /// Shift left
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="bits">Bits</param>
+        /// <returns>Value</returns>
+        public static short ShiftLeft(this short value, int bits) => (short)(value << bits);
+
+        /// <summary>
+        /// Shift left
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="bits">Bits</param>
+        /// <returns>Value</returns>
+        public static short ShiftRight(this short value, int bits) => (short)(value >> bits);
+
+        /// <summary>
+        /// Has flags?
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="flags">Flags</param>
+        /// <returns>Has the flags?</returns>
+        public static bool HasFlags(this short value, short flags) => (value & flags) == flags;
+
+        /// <summary>
+        /// Add flags
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="flags">Flags</param>
+        /// <returns>Value</returns>
+        public static short AddFlags(this short value, short flags) => (short)(value | flags);
+
+        /// <summary>
+        /// Remove flags
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="flags">Flags</param>
+        /// <returns>Value</returns>
+        public static short RemoveFlags(this short value, short flags) => (short)(value & ~flags);
+
+        /// <summary>
+        /// Cast as byte
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static byte ToByte(this short value) => (byte)value;
+
+        /// <summary>
+        /// Cast as signed byte
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static sbyte ToSByte(this short value) => (sbyte)value;
+
+        /// <summary>
+        /// Cast as unsigned short
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static ushort ToUShort(this short value) => (ushort)value;
+
+        /// <summary>
+        /// Cast as integer
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static int ToInt(this short value) => value;
+
+        /// <summary>
+        /// Cast as unsigned integer
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static uint ToUInt(this short value) => (uint)value;
+
+        /// <summary>
+        /// Cast as long
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static long ToLong(this short value) => value;
+
+        /// <summary>
+        /// Cast as unsigned long
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static ulong ToULong(this short value) => (ulong)value;
+    }
+}
---
+++ src/Wan24-Core/BitwiseUInt8Extensions.cs
@@ -0,0 +1,97 @@
+﻿namespace wan24.Core
+{
+    /// <summary>
+    /// Bitwise UInt8 extensions
+    /// </summary>
+    public static class BitwiseUInt8Extensions
+    {
+        /// <summary>
+        /// Shift left
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="bits">Bits</param>
+        /// <returns>Value</returns>
+        public static byte ShiftLeft(this byte value, int bits) => (byte)(value << bits);
+
+        /// <summary>
+        /// Shift left
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="bits">Bits</param>
+        /// <returns>Value</returns>
+        public static byte ShiftRight(this byte value, int bits) => (byte)(value >> bits);
+
+        /// <summary>
+        /// Has flags?
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="flags">Flags</param>
+        /// <returns>Has the flags?</returns>
+        public static bool HasFlags(this byte value, byte flags) => (value & flags) == flags;
+
+        /// <summary>
+        /// Add flags
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="flags">Flags</param>
+        /// <returns>Value</returns>
+        public static byte AddFlags(this byte value, byte flags) => (byte)(value | flags);
+
+        /// <summary>
+        /// Remove flags
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="flags">Flags</param>
+        /// <returns>Value</returns>
+        public static byte RemoveFlags(this byte value, byte flags) => (byte)(value & ~flags);
+
+        /// <summary>
+        /// Cast as signed byte
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static sbyte ToSByte(this byte value) => (sbyte)value;
+
+        /// <summary>
+        /// Cast as short
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static short ToShort(this byte value) => value;
+
+        /// <summary>
+        /// Cast as unsigned short
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static ushort ToUShort(this byte value) => value;
+
+        /// <summary>
+        /// Cast as integer
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static int ToInt(this byte value) => value;
+
+        /// <summary>
+        /// Cast as unsigned integer
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static uint ToUInt(this byte value) => value;
+
+        /// <summary>
+        /// Cast as long
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static long ToLong(this byte value) => value;
+
+        /// <summary>
+        /// Cast as unsigned long
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Value</returns>
+        public static ulong ToULong(this byte value) => value;
+    }
+}
