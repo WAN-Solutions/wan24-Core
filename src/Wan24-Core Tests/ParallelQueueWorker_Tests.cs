1a4969a30af329a181643c9e9f4fa17e771c51ae
nd <nd1012@users.noreply.github.com>
2023-06-03 13:21:36 +0200

    Update

--- src/Wan24-Core Tests/ParallelQueueWorker_Tests.cs
+++ src/Wan24-Core Tests/ParallelQueueWorker_Tests.cs
@@ -5,7 +5,7 @@ namespace Wan24_Core_Tests
     [TestClass]
     public class ParallelQueueWorker_Tests
     {
-        [TestMethod("ParallelQueueWorker_Tests.General_Tests"), Timeout(1000)]
+        [TestMethod("ParallelQueueWorker_Tests.General_Tests"), Timeout(3000)]
         public async Task General_Tests()
         {
             using ParallelQueueWorker worker = new(3, 2);
