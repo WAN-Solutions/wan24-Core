586fc93ea5bdc406326bcbe48c9c1e14e5464f81
nd <nd1012@users.noreply.github.com>
2023-09-15 21:29:08 +0200

    Update
    + Added `AsyncTimer`

---
+++ src/Wan24-Core Tests/AsyncTimer_Tests.cs
@@ -0,0 +1,27 @@
+﻿using wan24.Core;
+
+namespace Wan24_Core_Tests
+{
+    [TestClass]
+    public class AsyncTimer_Tests
+    {
+        [TestMethod]
+        public async Task GeneralAsync_Tests()
+        {
+            int runCount = 0;
+            using AsyncTimer timer = new(TimeSpan.FromMilliseconds(50), (timer) =>
+            {
+                runCount++;
+                return Task.CompletedTask;
+            });
+            await timer.StartAsync();
+            await Task.Delay(120);
+            Assert.AreEqual(2, runCount);
+            Assert.IsTrue(timer.IsRunning);
+            timer.AutoReset = false;
+            await Task.Delay(100);
+            Assert.AreEqual(3, runCount);
+            Assert.IsFalse(timer.IsRunning);
+        }
+    }
+}
