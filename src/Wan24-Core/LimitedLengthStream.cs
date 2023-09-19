ad788475191efc9d0b881a58002f2281d2b96ad8
nd <nd1012@users.noreply.github.com>
2023-09-19 11:56:04 +0200

    Update
    + `LengthLimitedStream` limits the reading-length of the base stream now, will avoid an invalid `Position`, and may cut `Length` to match the defined limit

--- src/Wan24-Core/LimitedLengthStream.cs
+++ src/Wan24-Core/LimitedLengthStream.cs
@@ -1,7 +1,7 @@
 ﻿namespace wan24.Core
 {
     /// <summary>
-    /// Limited length stream wrapper (limits writing only!)
+    /// Limited length stream wrapper
     /// </summary>
     public class LimitedLengthStream : LimitedLengthStream<Stream>
     {
@@ -15,7 +15,7 @@
     }
 
     /// <summary>
-    /// Limited length stream wrapper (limits writing only!)
+    /// Limited length stream wrapper
     /// </summary>
     /// <typeparam name="T">Wrapped stream type</typeparam>
     public class LimitedLengthStream<T> : WrapperStream<T> where T : Stream
@@ -37,6 +37,20 @@
         /// Maximum length in bytes
         /// </summary>
         public long MaxLength { get; set; }
+
+        /// <inheritdoc/>
+        public override long Position
+        {
+            get => base.Position;
+            set
+            {
+                if (value > MaxLength) throw new ArgumentOutOfRangeException(nameof(value));
+                base.Position = value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override long Length => Math.Min(base.Length, MaxLength);
 
         /// <summary>
         /// Detach the base stream and dispose
@@ -106,6 +120,56 @@
             EnsureUndisposed();
             if (Position + buffer.Length > MaxLength) throw new OutOfMemoryException("Maximum length exceeded");
             return _BaseStream.WriteAsync(buffer, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            EnsureUndisposed();
+            if (Position + count > MaxLength) count = (int)(MaxLength - Position);
+            return base.Read(buffer, offset, count);
+        }
+
+        /// <inheritdoc/>
+        public override int Read(Span<byte> buffer)
+        {
+            EnsureUndisposed();
+            if (Position + buffer.Length > MaxLength) buffer = buffer[..(buffer.Length - (int)(MaxLength - Position))];
+            return base.Read(buffer);
+        }
+
+        /// <inheritdoc/>
+        public override int ReadByte() => Position >= MaxLength ? -1 : base.ReadByte();
+
+        /// <inheritdoc/>
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            EnsureUndisposed();
+            if (Position + count > MaxLength) count = (int)(MaxLength - Position);
+            return base.ReadAsync(buffer, offset, count, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            EnsureUndisposed();
+            if (Position + buffer.Length > MaxLength) buffer = buffer[..(buffer.Length - (int)(MaxLength - Position))];
+            return base.ReadAsync(buffer, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            EnsureUndisposed();
+            long pos = origin switch
+            {
+                SeekOrigin.Begin => offset,
+                SeekOrigin.Current => Position + offset,
+                SeekOrigin.End => Length + offset,
+                _ => throw new ArgumentException("Invalid origin", nameof(origin))
+            };
+            if (pos > MaxLength) throw new ArgumentOutOfRangeException(nameof(offset));
+            return base.Seek(offset, origin);
         }
     }
 }
--- src/Wan24-Core/README.md
+++ src/Wan24-Core/README.md
@@ -711,7 +711,7 @@ public sealed class Config : OverrideableConfig<Config>
 
     private Config(Config parent, Config? sub = null) : base(parent)
     {
-        if(sub is not null)
+        if(sub != null)
         {
             SubConfig = sub;
             sub.ParentConfig = this;
@@ -725,7 +725,7 @@ public sealed class Config : OverrideableConfig<Config>
 
     private void InitProperties()
     {
-        AnyValue = ParentConfig is null 
+        AnyValue = ParentConfig == null 
             // The master option has a default value
             ? new(this, nameof(AnyValue), canBeOverridden: true, "default")
             // No default value for a sub-option
