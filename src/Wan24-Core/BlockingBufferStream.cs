dd95f194ae147496bed38c873414de524f5781ec
nd <nd1012@users.noreply.github.com>
2023-10-09 20:42:37 +0200

    Update
    + Added `TranslationHelper`
    + Added `BlockingInstancePool<T>`
    + Any `*InstancePool` prefers the asynchronous instance factory now and supports instance creation interruption using the internal `BufferSync` event
    + Added `BlockingBufferStream.TryWrite(Async)`

--- src/Wan24-Core/BlockingBufferStream.cs
+++ src/Wan24-Core/BlockingBufferStream.cs
@@ -453,6 +453,53 @@
             }
         }
 
+        /// <summary>
+        /// Try writing a buffer without blocking
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <returns>Number of bytes written</returns>
+        public virtual int TryWrite(ReadOnlySpan<byte> buffer)
+        {
+            EnsureUndisposed();
+            if (_IsEndOfFile) throw new InvalidOperationException();
+            int res = 0;
+            bool blocking = false,
+                haveData = false;
+            for (int write; buffer.Length != 0 && EnsureUndisposed();)
+            {
+                using (SemaphoreSyncContext ssc = BufferSync.SyncContext())
+                {
+                    EnsureUndisposed();
+                    write = Math.Min(SpaceLeft, buffer.Length);
+                    if (write == 0) return res;
+                    buffer[..write].CopyTo(Buffer.Span[WriteOffset..]);
+                    buffer = buffer[write..];
+                    WriteOffset += write;
+                    _Length += write;
+                    res += write;
+                    if (SpaceLeft == 0)
+                    {
+                        blocking = true;
+                        SpaceEvent.Reset();
+                    }
+                    haveData = !DataEvent.IsSet;
+                    DataEvent.Set();
+                }
+                if (haveData)
+                {
+                    RaiseOnDataAvailable();
+                    haveData = false;
+                }
+                if (blocking)
+                {
+                    RaiseOnNeedSpace();
+                    blocking = false;
+                }
+                if (AutoReorg && SpaceLeft == 0) ReorganizeBuffer();
+            }
+            return res;
+        }
+
         /// <inheritdoc/>
         public sealed override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
             => await WriteAsync(buffer.AsMemory(offset, count), cancellationToken).DynamicContext();
@@ -481,6 +528,7 @@
                         blocking = true;
                         SpaceEvent.Reset();
                     }
+                    haveData = !DataEvent.IsSet;
                     DataEvent.Set();
                 }
                 if (haveData)
@@ -495,6 +543,54 @@
                 }
                 if (AutoReorg && SpaceLeft == 0) await ReorganizeBufferAsync(cancellationToken).DynamicContext();
             }
+        }
+
+        /// <summary>
+        /// Try writing a buffer without blocking
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Number of bytes written</returns>
+        public virtual async ValueTask<int> TryWriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            EnsureUndisposed();
+            if (_IsEndOfFile) throw new InvalidOperationException();
+            int res = 0;
+            bool blocking = false,
+                haveData = false;
+            for (int write; buffer.Length != 0 && EnsureUndisposed();)
+            {
+                using (SemaphoreSyncContext ssc = await BufferSync.SyncContextAsync(cancellationToken).DynamicContext())
+                {
+                    EnsureUndisposed();
+                    write = Math.Min(SpaceLeft, buffer.Length);
+                    if (write == 0) return res;
+                    buffer.Span[..write].CopyTo(Buffer.Span[WriteOffset..]);
+                    buffer = buffer[write..];
+                    WriteOffset += write;
+                    _Length += write;
+                    res += write;
+                    if (SpaceLeft == 0)
+                    {
+                        blocking = true;
+                        SpaceEvent.Reset();
+                    }
+                    haveData = !DataEvent.IsSet;
+                    DataEvent.Set();
+                }
+                if (haveData)
+                {
+                    RaiseOnDataAvailable();
+                    haveData = false;
+                }
+                if (blocking)
+                {
+                    RaiseOnNeedSpace();
+                    blocking = false;
+                }
+                if (AutoReorg && SpaceLeft == 0) await ReorganizeBufferAsync(cancellationToken).DynamicContext();
+            }
+            return res;
         }
 
         /// <inheritdoc/>
