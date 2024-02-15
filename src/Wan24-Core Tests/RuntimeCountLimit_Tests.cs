aed2a8453666b9dcc866e14dbe94697eea030ec4
nd <nd1012@users.noreply.github.com>
2024-02-15 07:20:12 +0100

    Update
    + Added `RuntimeCountLimitAttribute`

---
+++ src/Wan24-Core Tests/RuntimeCountLimit_Tests.cs
@@ -0,0 +1,18 @@
+﻿using wan24.Core;
+
+namespace Wan24_Core_Tests
+{
+    [TestClass]
+    public class RuntimeCountLimit_Tests
+    {
+        public static int Limit { get; set; } = 123;
+
+        [TestMethod]
+        public void General_Tests()
+        {
+            RuntimeCountLimitAttribute attr = new("Wan24_Core_Tests.RuntimeCountLimit_Tests.Limit", "Wan24_Core_Tests.RuntimeCountLimit_Tests.Limit");
+            Assert.AreEqual(Limit, attr.Min);
+            Assert.AreEqual(Limit, attr.Max);
+        }
+    }
+}
