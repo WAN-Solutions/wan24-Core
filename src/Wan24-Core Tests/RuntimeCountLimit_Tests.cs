5114f3c7edb3433cc28c926598655d0b545004f1
nd <nd1012@users.noreply.github.com>
2024-02-17 10:02:16 +0100

    Dev (#58)
    + Added `RuntimeCountLimitAttribute`

---
+++ src/Wan24-Core Tests/RuntimeCountLimit_Tests.cs
@@ -0,0 +1,20 @@
+﻿using wan24.Core;
+
+namespace Wan24_Core_Tests
+{
+    [TestClass]
+    public class RuntimeCountLimit_Tests
+    {
+        public static int MinLimit { get; set; } = 123;
+
+        public static int MaxLimit { get; set; } = 999;
+
+        [TestMethod]
+        public void General_Tests()
+        {
+            RuntimeCountLimitAttribute attr = new("Wan24_Core_Tests.RuntimeCountLimit_Tests.MaxLimit", "Wan24_Core_Tests.RuntimeCountLimit_Tests.MinLimit");
+            Assert.AreEqual(MinLimit, attr.Min);
+            Assert.AreEqual(MaxLimit, attr.Max);
+        }
+    }
+}
---
+++ src/Wan24-Core/RuntimeCountLimitAttribute.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace wan24.Core
+{
+    /// <summary>
+    /// Runtime count limitation attribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RuntimeCountLimitAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Minimum
+        /// </summary>
+        protected readonly long? _Min = null;
+        /// <summary>
+        /// Minimum getter
+        /// </summary>
+        protected readonly PropertyInfoExt? _MinGetter = null;
+        /// <summary>
+        /// Maximum getter
+        /// </summary>
+        protected readonly PropertyInfoExt _MaxGetter = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="maxGetter">Maximum getter static property</param>
+        /// <param name="minGetter">Minimum getter static property</param>
+        /// <param name="min">Minimum</param>
+        public RuntimeCountLimitAttribute(string maxGetter, string? minGetter = null, long? min = null) : base()
+        {
+            _Min = min;
+            MinGetter = minGetter;
+            MaxGetter = maxGetter;
+            string[] info;
+            if (minGetter is not null)
+            {
+                info = minGetter.Split('.');
+                if (info.Length == 1) throw new ArgumentException("Invalid min. getter", nameof(minGetter));
+                Type type = TypeHelper.Instance.GetType(string.Join('.', info.Take(info.Length - 1)))
+                    ?? throw new ArgumentException($"Can't load type \"{string.Join('.', info.Take(info.Length - 1))}\"", nameof(minGetter));
+                _MinGetter = type.GetPropertyCached(info[^1], BindingFlags.Public | BindingFlags.Static)
+                    ?? throw new ArgumentException("Can't load public static property", nameof(minGetter));
+            }
+            {
+                info = maxGetter.Split('.');
+                if (info.Length == 1) throw new ArgumentException("Invalid max. getter", nameof(maxGetter));
+                Type type = TypeHelper.Instance.GetType(string.Join('.', info.Take(info.Length - 1)))
+                    ?? throw new ArgumentException($"Can't load type \"{string.Join('.', info.Take(info.Length - 1))}\"", nameof(maxGetter));
+                _MaxGetter = type.GetPropertyCached(info[^1], BindingFlags.Public | BindingFlags.Static)
+                    ?? throw new ArgumentException("Can't load public static property", nameof(maxGetter));
+            }
+        }
+
+        /// <summary>
+        /// Minimum
+        /// </summary>
+        public long? Min
+        {
+            get
+            {
+                if (_MinGetter is null) return _Min;
+                object? value = _MinGetter.Getter?.Invoke(arg: null);
+                if (value is null) return null;
+                return (long)Convert.ChangeType(value, typeof(long));
+            }
+        }
+
+        /// <summary>
+        /// Maximum
+        /// </summary>
+        public long Max => (long)Convert.ChangeType(_MaxGetter.Getter?.Invoke(arg: null) ?? throw new InvalidProgramException("Maximum getter returned NULL"), typeof(long));
+
+        /// <summary>
+        /// Minimum getter static property
+        /// </summary>
+        public string? MinGetter { get; }
+
+        /// <summary>
+        /// Maximum getter static property
+        /// </summary>
+        public string MaxGetter { get; }
+
+        /// <summary>
+        /// Get the error message
+        /// </summary>
+        /// <param name="count">Count</param>
+        /// <param name="member">Member</param>
+        /// <returns>Error message</returns>
+        public string? GetErrorMessage(long? count, string? member)
+            => count is null || ((Min is null || count >= Min) && count <= Max)
+                ? null
+                : member is null
+                        ? (Min is null
+                            ? ErrorMessage ?? $"Maximum count is {Max} ({count})"
+                            : ErrorMessage ?? $"Count must be between {Min} and {Max} ({count})")
+                        : (Min is null
+                            ? (ErrorMessage is null
+                                ? $"Maximum count of the property {member} is {Max} ({count})"
+                                : $"{member}: {ErrorMessage}")
+                            : (ErrorMessage is null
+                                ? $"Count of the property {member} must be between {Min} and {Max} ({count})"
+                                : $"{member}: {ErrorMessage}"));
+
+        /// <inheritdoc/>
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+            => GetErrorMessage(
+                    value switch
+                    {
+                        IDictionary dict => dict.Count,
+                        Array arr => arr.LongLength,
+                        IList list => list.Count,
+                        ICollection col => col.Count,
+                        _ => null
+                    },
+                    validationContext.MemberName
+                    ) is string error
+                ? new ValidationResult(error, validationContext.MemberName is null ? null : new string[] { validationContext.MemberName })
+                : null;
+    }
+}
