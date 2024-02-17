ff92a3ea1d7bc31fb8489fbe7c1292ae4757e1c6
nd <nd1012@users.noreply.github.com>
2024-02-17 17:52:27 +0100

    Update PortKnocking.cs

--- src/Wan24-Core/PortKnocking.cs
+++ src/Wan24-Core/PortKnocking.cs
@@ -13,9 +13,10 @@ namespace wan24.Core
         /// </summary>
         /// <param name="target">Target IP address</param>
         /// <param name="timeout">Timeout between packets</param>
+        /// <param name="progress">Progress (updated after each port was contacted)</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <param name="ports">TCP port sequence</param>
-        public static async Task CallTcpSynSequenceAsync(IPAddress target, TimeSpan timeout, CancellationToken cancellationToken = default, params int[] ports)
+        public static async Task CallTcpSynSequenceAsync(IPAddress target, TimeSpan timeout, ProcessingProgress? progress = null, CancellationToken cancellationToken = default, params int[] ports)
         {
             if (ports.Length < 1 || ports.Any(p => p < 1 || p > ushort.MaxValue)) throw new ArgumentOutOfRangeException(nameof(ports));
             using CancellationTokenSource cts = new();
@@ -27,36 +28,43 @@ namespace wan24.Core
                 {
                     try
                     {
-                        cts.TryReset();
+                        try
+                        {
+                            cts.TryReset();
+                            if (timeout != TimeSpan.Zero)
+                            {
+                                using BoundCancellationTokenSource bcts = new(cancellationToken, cts.Token);
+                                cts.CancelAfter(timeout);
+                                started = DateTime.Now;
+                                // Can't use raw sockets to send a SYN packet here 'cause of possible restrictions using raw sockets
+                                await server.ConnectAsync(new(target, port), bcts.Token).DynamicContext();
+                            }
+                            else
+                            {
+                                // Can't use raw sockets to send a SYN packet here 'cause of possible restrictions using raw sockets
+                                await server.ConnectAsync(new(target, port), cancellationToken).DynamicContext();
+                            }
+                            server.Dispose();
+                            server = new();
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            if (timeout == TimeSpan.Zero) throw;
+                            cancellationToken.ThrowIfCancellationRequested();
+                            continue;
+                        }
+                        catch
+                        {
+                        }
                         if (timeout != TimeSpan.Zero)
                         {
-                            using BoundCancellationTokenSource bcts = new(cancellationToken, cts.Token);
-                            cts.CancelAfter(timeout);
-                            started = DateTime.Now;
-                            // Can't use raw sockets to send a SYN packet here 'cause of possible restrictions using raw sockets
-                            await server.ConnectAsync(new(target, port), bcts.Token).DynamicContext();
+                            DateTime end = started + timeout;
+                            if (end > DateTime.Now) await Task.Delay(end - DateTime.Now, cancellationToken).DynamicContext();
                         }
-                        else
-                        {
-                            // Can't use raw sockets to send a SYN packet here 'cause of possible restrictions using raw sockets
-                            await server.ConnectAsync(new(target, port), cancellationToken).DynamicContext();
-                        }
-                        server.Dispose();
-                        server = new();
                     }
-                    catch (OperationCanceledException)
+                    finally
                     {
-                        if (timeout == TimeSpan.Zero) throw;
-                        cancellationToken.ThrowIfCancellationRequested();
-                        continue;
-                    }
-                    catch
-                    {
-                    }
-                    if (timeout != TimeSpan.Zero)
-                    {
-                        DateTime end = started + timeout;
-                        if (end > DateTime.Now) await Task.Delay(end - DateTime.Now, cancellationToken).DynamicContext();
+                        progress?.Update();
                     }
                 }
             }
@@ -71,9 +79,10 @@ namespace wan24.Core
         /// </summary>
         /// <param name="target">Target IP address</param>
         /// <param name="timeout">Timeout between packets</param>
+        /// <param name="progress">Progress (updated after each port was contacted)</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <param name="ports">UDP port sequence</param>
-        public static async Task CallUdpEmptySequenceAsync(IPAddress target, TimeSpan timeout, CancellationToken cancellationToken = default, params int[] ports)
+        public static async Task CallUdpEmptySequenceAsync(IPAddress target, TimeSpan timeout, ProcessingProgress? progress = null, CancellationToken cancellationToken = default, params int[] ports)
         {
             if (ports.Length < 1 || ports.Any(p => p < 1 || p > ushort.MaxValue)) throw new ArgumentOutOfRangeException(nameof(ports));
             ReadOnlyMemory<byte> empty = Array.Empty<byte>();
@@ -82,6 +91,7 @@ namespace wan24.Core
             {
                 await server.SendAsync(empty, new(target, port), cancellationToken).DynamicContext();
                 if (timeout != TimeSpan.Zero) await Task.Delay(timeout, cancellationToken).DynamicContext();
+                progress?.Update();
             }
         }
     }
