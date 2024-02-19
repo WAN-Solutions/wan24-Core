b5f5928e8141d35191db70cd4e175849bb852c44
nd <nd1012@users.noreply.github.com>
2024-02-19 20:05:20 +0100

    Update
    + Added `DebugLogger.BreakDebuggerOnLevel `
    - `DebugLogger` won't write a message, if no debugger is attached

--- src/Wan24-Core/DebugLogger.cs
+++ src/Wan24-Core/DebugLogger.cs
@@ -13,8 +13,17 @@ namespace wan24.Core
     /// <param name="next">Next logger which should receive the message</param>
     public sealed class DebugLogger(in LogLevel? level = null, in ILogger? next = null) : LoggerBase(level, next)
     {
+        /// <summary>
+        /// Log level which will break the debugger
+        /// </summary>
+        public LogLevel BreakDebuggerOnLevel { get; set; } = LogLevel.None;
+
         /// <inheritdoc/>
         protected override void LogInt<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-            => Debug.WriteLine(GetMessage(logLevel, eventId, state, exception, formatter));
+        {
+            if (!Debugger.IsAttached) return;
+            Debug.WriteLine(GetMessage(logLevel, eventId, state, exception, formatter));
+            if (BreakDebuggerOnLevel != LogLevel.None && logLevel >= BreakDebuggerOnLevel) Debugger.Break();
+        }
     }
 }
