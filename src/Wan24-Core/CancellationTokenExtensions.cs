6159da312972f9dfa39342521e1b2618db374fc0
nd <nd1012@users.noreply.github.com>
2024-02-26 21:02:45 +0100

    Update
    + Added `EventWorkerBase`
    + Added `CancellationTokenExtensions.GetIsCancellationRequested`

--- src/Wan24-Core/CancellationTokenExtensions.cs
+++ src/Wan24-Core/CancellationTokenExtensions.cs
@@ -11,5 +11,17 @@
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>Cancellation awaiter</returns>
         public static CancellationAwaiter GetAwaiter(this CancellationToken cancellationToken) => new(cancellationToken);
+
+        /// <summary>
+        /// Get if cancellation is requested and throw an exception, if canceled.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <exception cref="OperationCanceledException">Cancellation is requested</exception>
+        /// <returns><see langword="false"/></returns>
+        public static bool GetIsCancellationRequested(this CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return false;
+        }
     }
 }
