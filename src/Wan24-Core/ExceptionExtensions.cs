522acbc6f37f69b793eed98eab0a0fe1029b3b71
nd <nd1012@users.noreply.github.com>
2024-05-19 11:08:20 +0200

    Update
    + Added `ExceptionExtensions`

---
+++ src/Wan24-Core/ExceptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime;
+
+namespace wan24.Core
+{
+    /// <summary>
+    /// <see cref="Exception"/> extensions
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Append exception(s)
+        /// </summary>
+        /// <param name="exception">Exception</param>
+        /// <param name="ex">Exception(s) to append</param>
+        /// <returns>Aggregate exception</returns>
+        [TargetedPatchingOptOut("Tiny method")]
+        public static AggregateException AppendException(this Exception exception, params Exception[] ex)
+            => exception is AggregateException aex
+                ? new([.. aex.InnerExceptions, .. ex])
+                : new([exception, .. ex]);
+    }
+}
