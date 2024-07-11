f1a14b6da3e0b6014b4f7db3ea14909caa504020
nd <nd1012@users.noreply.github.com>
2024-07-11 22:55:03 +0200

    Update
    + `Type(Name)Cache` adds basic CLR types to the cache per default now

--- src/Wan24-Core/TypeNameCache.cs
+++ src/Wan24-Core/TypeNameCache.cs
@@ -13,6 +13,33 @@ namespace wan24.Core
         public static readonly ConcurrentDictionary<int, Type> Types = [];
 
         /// <summary>
+        /// Constructor
+        /// </summary>
+        static TypeNameCache() => Add(
+            typeof(bool),
+            typeof(sbyte),
+            typeof(byte),
+            typeof(short),
+            typeof(ushort),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(Half),
+            typeof(float),
+            typeof(double),
+            typeof(decimal),
+            typeof(string),
+            typeof(byte[]),
+            typeof(int[]),
+            typeof(long[]),
+            typeof(float[]),
+            typeof(double[]),
+            typeof(decimal[]),
+            typeof(string[])
+            );
+
+        /// <summary>
         /// Add types to the <see cref="Types"/> cache
         /// </summary>
         /// <param name="types">Types</param>
