5d0a46145ee8d89260d3a66f72f501397b29fa48
nd <nd1012@users.noreply.github.com>
2024-07-13 12:26:55 +0200

    Update (#96)
    * `TypeCache.Types` is internal now
    + Added `EnumerableExtensions.WhereNotNull(Async)`
    + Added `OneOrMany<T>`
    + Added `CachedType`
    + Added unsafe `BytesExtensions.And/Xor/RotatingXor` variants
    + `Type(Name)Cache` adds basic CLR types to the cache per default now
    + Added `XY(Z)Int`
    + Added `EnumerableExtensions.CollectXY(Z)Int(Async)`
    + Added `TypeCache.Contains/GetTypeByHashCode/GetTypeByNameHashCode`

--- src/Wan24-Core/TypeCache.cs
+++ src/Wan24-Core/TypeCache.cs
@@ -10,15 +10,71 @@ namespace wan24.Core
         /// <summary>
         /// Type cache (key is the types hash code)
         /// </summary>
-        public static readonly ConcurrentDictionary<int, Type> Types = new();
+        internal static readonly ConcurrentDictionary<int, Type> Types = [];
+        /// <summary>
+        /// Type cache (key is the types name (see <see cref="Type.ToString"/>) hash code)
+        /// </summary>
+        internal static readonly ConcurrentDictionary<int, Type> TypeNames = [];
 
         /// <summary>
-        /// Add types to the <see cref="Types"/> cache
+        /// Constructor
+        /// </summary>
+        static TypeCache() => Add(
+            typeof(bool),
+            typeof(sbyte),
+            typeof(byte),
+            typeof(short),
+            typeof(ushort),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(Half),
+            typeof(float),
+            typeof(double),
+            typeof(decimal),
+            typeof(string),
+            typeof(byte[]),
+            typeof(int[]),
+            typeof(long[]),
+            typeof(float[]),
+            typeof(double[]),
+            typeof(decimal[]),
+            typeof(string[])
+            );
+
+        /// <summary>
+        /// Add types to the <see cref="Types"/> and the <see cref="TypeNames"/> cache
         /// </summary>
         /// <param name="types">Types</param>
         public static void Add(params Type[] types)
         {
-            foreach(Type type in types) Types[type.GetHashCode()] = type;
+            foreach (Type type in types)
+            {
+                Types[type.GetHashCode()] = type;
+                TypeNames[type.ToString().GetHashCode()] = type;
+            }
         }
+
+        /// <summary>
+        /// Determine if a type was cached
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <returns></returns>
+        public static bool Contains(in Type type) => Types.ContainsKey(type.GetHashCode());
+
+        /// <summary>
+        /// Get a cached type by its hash code (see <see cref="Type.GetHashCode"/>)
+        /// </summary>
+        /// <param name="hashCode">Hash code (see <see cref="Type.GetHashCode"/>)</param>
+        /// <returns>Type</returns>
+        public static Type? GetTypeByHashCode(in int hashCode) => Types.TryGetValue(hashCode, out Type? res) ? res : null;
+
+        /// <summary>
+        /// Get a cached type by its hash code (see <see cref="Type.ToString"/>)
+        /// </summary>
+        /// <param name="hashCode">Name hash code (see <see cref="Type.ToString"/>)</param>
+        /// <returns>Type</returns>
+        public static Type? GetTypeByNameHashCode(in int hashCode) => TypeNames.TryGetValue(hashCode, out Type? res) ? res : null;
     }
 }
--- src/Wan24-Core/XYInt.cs
+++ src/Wan24-Core/XYInt.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 namespace wan24.Core
 {
     /// <summary>
-    /// XYZ value
+    /// XY value
     /// </summary>
     [StructLayout(LayoutKind.Explicit)]
-    public readonly record struct XYZ
+    public readonly record struct XYInt
     {
         /// <summary>
         /// Structure size in bytes
         /// </summary>
-        public const int STRUCTURE_SIZE = Z_FIELD_OFFSET + Z_FIELD_SIZE;
+        public const int STRUCTURE_SIZE = Y_FIELD_OFFSET + Y_FIELD_SIZE;
         /// <summary>
         /// X value field byte offset
         /// </summary>
@@ -21,7 +21,7 @@ namespace wan24.Core
         /// <summary>
         /// X value field size in bytes
         /// </summary>
-        public const int X_FIELD_SIZE = sizeof(double);
+        public const int X_FIELD_SIZE = sizeof(int);
         /// <summary>
         /// Y value field byte offset
         /// </summary>
@@ -29,96 +29,61 @@ namespace wan24.Core
         /// <summary>
         /// Y value field size in bytes
         /// </summary>
-        public const int Y_FIELD_SIZE = sizeof(double);
-        /// <summary>
-        /// Z value field byte offset
-        /// </summary>
-        public const int Z_FIELD_OFFSET = Y_FIELD_OFFSET + Y_FIELD_SIZE;
-        /// <summary>
-        /// Z value field size in bytes
-        /// </summary>
-        public const int Z_FIELD_SIZE = sizeof(double);
+        public const int Y_FIELD_SIZE = sizeof(int);
 
         /// <summary>
-        /// Zero X/Y/Z
+        /// Zero X/Y
         /// </summary>
-        public static readonly XYZ Zero = new();
+        public static readonly XYInt Zero = new();
         /// <summary>
         /// Minimum value
         /// </summary>
-        public static readonly XYZ MinValue = new(double.MinValue, double.MinValue, double.MinValue);
+        public static readonly XYInt MinValue = new(int.MinValue, int.MinValue);
         /// <summary>
         /// Maximum value
         /// </summary>
-        public static readonly XYZ MaxValue = new(double.MaxValue, double.MaxValue, double.MaxValue);
+        public static readonly XYInt MaxValue = new(int.MaxValue, int.MaxValue);
 
         /// <summary>
-        /// X value (real number)
+        /// X value
         /// </summary>
         [FieldOffset(X_FIELD_OFFSET)]
-        public readonly double X;
+        public readonly int X;
         /// <summary>
-        /// Y value (real number)
+        /// Y value
         /// </summary>
         [FieldOffset(Y_FIELD_OFFSET)]
-        public readonly double Y;
-        /// <summary>
-        /// Z value (real number)
-        /// </summary>
-        [FieldOffset(Z_FIELD_OFFSET)]
-        public readonly double Z;
+        public readonly int Y;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public XYZ()
+        public XYInt()
         {
             X = 0;
             Y = 0;
-            Z = 0;
         }
 
         /// <summary>
         /// Constructor
         /// </summary>
-        /// <param name="x">X value (real number)</param>
-        /// <param name="y">Y value (real number)</param>
-        /// <param name="z">Z value (real number)</param>
-        public XYZ(in double x, in double y, in double z)
+        /// <param name="x">X value</param>
+        /// <param name="y">Y value</param>
+        public XYInt(in int x, in int y)
         {
-            if (double.IsNaN(x) || !double.IsFinite(x)) throw new ArgumentException("Invalid value", nameof(x));
-            if (double.IsNaN(y) || !double.IsFinite(y)) throw new ArgumentException("Invalid value", nameof(y));
-            if (double.IsNaN(z) || !double.IsFinite(z)) throw new ArgumentException("Invalid value", nameof(z));
             X = x;
             Y = y;
-            Z = z;
         }
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="data">Serialized data (min. <see cref="STRUCTURE_SIZE"/> bytes length required)</param>
-        public XYZ(in ReadOnlySpan<byte> data)
+        public XYInt(in ReadOnlySpan<byte> data)
         {
             if (data.Length < STRUCTURE_SIZE) throw new ArgumentOutOfRangeException(nameof(data));
-            X = data.ToDouble();
-            if (double.IsNaN(X) || !double.IsFinite(X)) throw new InvalidDataException("Invalid X value");
-            Y = data[Y_FIELD_OFFSET..].ToDouble();
-            if (double.IsNaN(Y) || !double.IsFinite(Y)) throw new InvalidDataException("Invalid Y value");
-            Z = data[Z_FIELD_OFFSET..].ToDouble();
-            if (double.IsNaN(Z) || !double.IsFinite(Z)) throw new InvalidDataException("Invalid Z value");
-        }
-
-        /// <summary>
-        /// Get the values as <see cref="XY"/>
-        /// </summary>
-        public XY AsXy
-        {
-            [TargetedPatchingOptOut("Tiny method")]
-#if !NO_INLINE
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-            get => new(X, Y);
+            X = data.ToInt();
+            Y = data[Y_FIELD_OFFSET..].ToInt();
         }
 
         /// <summary>
@@ -150,7 +115,6 @@ namespace wan24.Core
             if (buffer.Length < STRUCTURE_SIZE) throw new ArgumentOutOfRangeException(nameof(buffer));
             X.GetBytes(buffer);
             Y.GetBytes(buffer[Y_FIELD_OFFSET..]);
-            Z.GetBytes(buffer[Z_FIELD_OFFSET..]);
             return buffer;
         }
 
@@ -159,17 +123,17 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public override string ToString() => $"{X};{Y};{Z}";
+        public override string ToString() => $"{X};{Y}";
 
         /// <summary>
         /// Cast as serialized data
         /// </summary>
-        /// <param name="xyz">XYZ</param>
+        /// <param name="xy">XY</param>
         [TargetedPatchingOptOut("Just a method adapter")]
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator byte[](in XYZ xyz) => xyz.GetBytes();
+        public static implicit operator byte[](in XYInt xy) => xy.GetBytes();
 
         /// <summary>
         /// Cast from serialized data
@@ -179,7 +143,7 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator XYZ(in byte[] data) => new(data);
+        public static implicit operator XYInt(in byte[] data) => new(data);
 
         /// <summary>
         /// Cast from serialized data
@@ -189,7 +153,7 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator XYZ(in Span<byte> data) => new(data);
+        public static implicit operator XYInt(in Span<byte> data) => new(data);
 
         /// <summary>
         /// Cast from serialized data
@@ -199,7 +163,7 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator XYZ(in ReadOnlySpan<byte> data) => new(data);
+        public static implicit operator XYInt(in ReadOnlySpan<byte> data) => new(data);
 
         /// <summary>
         /// Cast from serialized data
@@ -209,7 +173,7 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator XYZ(in Memory<byte> data) => new(data.Span);
+        public static implicit operator XYInt(in Memory<byte> data) => new(data.Span);
 
         /// <summary>
         /// Cast from serialized data
@@ -219,27 +183,27 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator XYZ(in ReadOnlyMemory<byte> data) => new(data.Span);
+        public static implicit operator XYInt(in ReadOnlyMemory<byte> data) => new(data.Span);
 
         /// <summary>
         /// Cast as <see cref="string"/>
         /// </summary>
-        /// <param name="xy"><see cref="XY"/></param>
+        /// <param name="xy"><see cref="XYInt"/></param>
         [TargetedPatchingOptOut("Just a method adapter")]
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator string(in XYZ xy) => xy.ToString();
+        public static implicit operator string(in XYInt xy) => xy.ToString();
 
         /// <summary>
         /// Cast as <see cref="string"/>
         /// </summary>
-        /// <param name="xy"><see cref="XY"/></param>
+        /// <param name="xy"><see cref="XYInt"/></param>
         [TargetedPatchingOptOut("Just a method adapter")]
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator ReadOnlySpan<char>(in XYZ xy) => xy.ToString();
+        public static implicit operator ReadOnlySpan<char>(in XYInt xy) => xy.ToString();
 
         /// <summary>
         /// Cast from a <see cref="string"/>
@@ -249,7 +213,7 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator XYZ(in ReadOnlySpan<char> str) => Parse(str);
+        public static implicit operator XYInt(in ReadOnlySpan<char> str) => Parse(str);
 
         /// <summary>
         /// Cast from a <see cref="string"/>
@@ -259,67 +223,52 @@ namespace wan24.Core
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator XYZ(in string str) => Parse(str);
+        public static implicit operator XYInt(in string str) => Parse(str);
 
         /// <summary>
         /// Cast as <see cref="double"/> array
         /// </summary>
-        /// <param name="xyz"><see cref="XYZ"/></param>
+        /// <param name="xy"><see cref="XYInt"/></param>
         [TargetedPatchingOptOut("Tiny method")]
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static implicit operator double[](in XYZ xyz) => [xyz.X, xyz.Y, xyz.Z];
-
-        /// <summary>
-        /// Cas as <see cref="XY"/>
-        /// </summary>
-        /// <param name="xyz"><see cref="XYZ"/></param>
-        [TargetedPatchingOptOut("Just a method adapter")]
-#if !NO_INLINE
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-        public static implicit operator XY(in XYZ xyz) => xyz.AsXy;
+        public static implicit operator double[](in XYInt xy) => [xy.X, xy.Y];
 
         /// <summary>
         /// Parse a string
         /// </summary>
         /// <param name="str">String</param>
-        /// <returns><see cref="XYZ"/></returns>
+        /// <returns><see cref="XYInt"/></returns>
         [TargetedPatchingOptOut("Tiny method")]
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static XYZ Parse(in ReadOnlySpan<char> str)
+        public static XYInt Parse(in ReadOnlySpan<char> str)
         {
-            int index1 = str.IndexOf(';');
-            if (index1 < 1) throw new FormatException("No value separator found in input string");
-            int index2 = str[(index1 + 1)..].IndexOf(';');
-            if (index2 < 1) throw new FormatException("No second value separator found in input string");
-            return new(double.Parse(str[..index1]), double.Parse(str.Slice(index1 + 1, index2)), double.Parse(str[(index1 + index2 + 2)..]));
+            int index = str.IndexOf(';');
+            if (index < 1) throw new FormatException("No value separator found in input string");
+            return new(int.Parse(str[..index]), int.Parse(str[(index + 1)..]));
         }
 
         /// <summary>
         /// Try parsing a string
         /// </summary>
         /// <param name="str">String</param>
-        /// <param name="result"><see cref="XY"/></param>
+        /// <param name="result"><see cref="XYInt"/></param>
         /// <returns>If succeed</returns>
         [TargetedPatchingOptOut("Tiny method")]
 #if !NO_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static bool TryParse(in ReadOnlySpan<char> str, out XYZ result)
+        public static bool TryParse(in ReadOnlySpan<char> str, out XYInt result)
         {
             result = default;
-            int index1 = str.IndexOf(';');
-            if (index1 < 1) return false;
-            int index2 = str[(index1 + 1)..].IndexOf(';');
-            if (index2 < 1) return false;
-            if (!double.TryParse(str[..index1], out double x) || double.IsNaN(x) || !double.IsFinite(x)) return false;
-            if (!double.TryParse(str.Slice(index1 + 1, index2), out double y) || double.IsNaN(y) || !double.IsFinite(y)) return false;
-            if (!double.TryParse(str[(index1 + index2 + 2)..], out double z) || double.IsNaN(z) || !double.IsFinite(z)) return false;
-            result = new(x, y, z);
+            int index = str.IndexOf(';');
+            if (index < 1) return false;
+            if (!int.TryParse(str[..index], out int x)) return false;
+            if (!int.TryParse(str[(index + 1)..], out int y)) return false;
+            result = new(x, y);
             return true;
         }
     }
