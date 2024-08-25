e1cfabe9acdebaadd18da89aa52e91d6d6800184
nd <nd1012@users.noreply.github.com>
2024-08-25 16:58:57 +0700

    Update EnumInfo_Tests.cs

--- src/Wan24-Core Benchmark Tests/EnumInfo_Tests.cs
+++ src/Wan24-Core Benchmark Tests/EnumInfo_Tests.cs
@@ -17,7 +17,7 @@ namespace Wan24_Core_Benchmark_Tests
         public void NetGetValues() => Enum.GetValues<TestEnum>();
 
         [Benchmark]
-        public void Wan24GetValues() => _ = EnumInfo<TestEnum>.Values;
+        public IReadOnlySet<TestEnum> Wan24GetValues() => EnumInfo<TestEnum>.Values;
 
         [Benchmark]
         public void FastEnumGetValues() => FastEnum.GetValues<TestEnum>();
@@ -26,7 +26,7 @@ namespace Wan24_Core_Benchmark_Tests
         public void NetGetNames() => Enum.GetNames<TestEnum>();
 
         [Benchmark]
-        public void Wan24GetNames() => _ = EnumInfo<TestEnum>.Names;
+        public IReadOnlySet<string> Wan24GetNames() => EnumInfo<TestEnum>.Names;
 
         [Benchmark]
         public void FastEnumGetNames() => FastEnum.GetNames<TestEnum>();
@@ -53,7 +53,7 @@ namespace Wan24_Core_Benchmark_Tests
         public void NetIsDefined() => Enum.IsDefined(TestEnum.Value0);
 
         [Benchmark]
-        public void Wan24IsDefined() => TestEnum.Value0.IsValidEnumerationValue();
+        public bool Wan24IsDefined() => EnumInfo<TestEnum>.Values.Contains(TestEnum.Value0) || EnumInfo<TestEnum>.FlagValues.Contains(TestEnum.Value0);
 
         [Benchmark]
         public void FatsEnumIsValid() => FastEnum.IsDefined(TestEnum.Value0);
@@ -62,7 +62,7 @@ namespace Wan24_Core_Benchmark_Tests
         public void NetParse() => Enum.Parse<TestEnum>("Value0");
 
         [Benchmark]
-        public void Wan24Parse() => _ = EnumInfo<TestEnum>.KeyValues["Value0"];
+        public void Wan24Parse() => EnumHelper.ParseEnum<TestEnum>("Value0");
 
         [Benchmark]
         public void FastEnumParse() => FastEnum.Parse<TestEnum>("Value0");
@@ -71,16 +71,16 @@ namespace Wan24_Core_Benchmark_Tests
         public void NetTryParse() => Enum.TryParse<TestEnum>("Value0", out _);
 
         [Benchmark]
-        public void Wan24TryParse() => EnumInfo<TestEnum>.KeyValues.TryGetValue("Value0", out _);
+        public void Wan24TryParse() => EnumHelper.TryParseEnum<TestEnum>("Value0", out _);
 
         [Benchmark]
         public void FastEnumTryParse() => FastEnum.TryParse<TestEnum>("Value0", out _);
 
-        private enum TestEnum
+        public enum TestEnum
         {
-            Value0 = 0,
-            Value1 = 1,
-            Value2 = 2
+            Value0,
+            Value1,
+            Value2
         }
     }
 }
